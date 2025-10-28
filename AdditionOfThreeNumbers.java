a284dd0be5c6d1fcb28fb1104082b56a58a81b25
Anshika Agarwal <anshikaagarwal2610@gmail.com>
2025-10-28 18:52:24 +0530

    my first java program

---
+++ AdditionOfThreeNumbers.java
@@ -0,0 +1,15 @@
+package com.company;
+import java.util.Scanner;
+
+public class AdditionOfThreeNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter three integers: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int sum = a + b + c;
+        System.out.println("Sum = " + sum);
+        sc.close();
+    }
+}
